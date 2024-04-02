2f732e3a9e727dba40f5dac51cf39d27156af517
Wojciech Zawadzki <zawadzki1993@hotmail.com>
2024-04-02 20:19:31 +0200

    Add project files.

---
+++ Cwiczenia.2PetlaConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+	static void Main()
+	{
+		Console.WriteLine("Liczby parzyste w zakresie od 0 do 1000:");
+
+		int number = 0;
+
+		do
+		{
+			if (number % 2 == 0)
+			{
+				Console.WriteLine(number);
+			}
+
+			number++;
+		} while (number <= 1000);
+	}
+}
